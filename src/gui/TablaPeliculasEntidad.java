c8adeca3a539cbd5c937bc14a2b6eaff0e0a8b75
Iker Villarroya <i.villarroya@opendeusto.es>
2025-11-17 09:59:08 +0100

    imagen de las peliculas

--- src/gui/TablaPeliculasEntidad.java
+++ src/gui/TablaPeliculasEntidad.java
@@ -1,12 +1,19 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.util.List;
+import java.awt.Component;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import dao.PeliculaDAO;
@@ -52,6 +59,36 @@ public class TablaPeliculasEntidad extends JFrame {
         };
 
         tablaPeliculas = new JTable(modeloTabla);
+        
+        tablaPeliculas.setGridColor(Color.LIGHT_GRAY);
+        tablaPeliculas.setFillsViewportHeight(true);
+        tablaPeliculas.setSelectionBackground(new Color(220,235,252));
+        tablaPeliculas.setSelectionForeground(Color.BLACK);
+        
+        DefaultTableCellRenderer rendererImagen = new DefaultTableCellRenderer() {
+        	 @Override
+             public Component getTableCellRendererComponent(
+                     JTable table, Object value, boolean isSelected,boolean hasFocus, int row, int column) {
+        		 JLabel label = (JLabel) super.getTableCellRendererComponent(table, "", isSelected, hasFocus, row, column);
+        		 label.setHorizontalAlignment(SwingConstants.CENTER);
+        		 
+        		 if (value != null) {
+        			 try {
+        				 ImageIcon original = new ImageIcon(value.toString());
+        				 Image imgEscalada = original.getImage().getScaledInstance(60, 80,  Image.SCALE_SMOOTH);
+        				 label.setIcon(new ImageIcon(imgEscalada));
+        				 
+        			 }catch (Exception e) {
+        				 label.setIcon(null);
+        			 }
+        		 }else {
+        			 label.setIcon(null);
+        		 }
+        		 return label;
+        	 }
+        };
+        
+        tablaPeliculas.getColumnModel().getColumn(6).setCellRenderer(rendererImagen);
 
         JScrollPane scrollPane = new JScrollPane(tablaPeliculas);
 
