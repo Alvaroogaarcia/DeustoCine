fb6a2f1b460aaab43990a15a574c677ec61c7f14
Iker Villarroya <i.villarroya@opendeusto.es>
2025-12-02 19:23:11 +0100

    descuentoDAO

---
+++ src/dao/DescuentoDAO.java
@@ -0,0 +1,52 @@
+package dao;
+
+import database.DBConnection;
+import domain.DescuentoPelicula;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DescuentoDAO {
+	
+	public boolean existePorId(String id) {
+		String sql = "SELECT COUNT(*) FROM descuento WHERE id= ?";
+		try (Connection conn = DBConnection.getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql)){
+			ps.setString(1, id);
+			try (ResultSet rs = ps.executeQuery()){
+				if (rs.next()) {
+					return rs.getInt(1) > 0;
+				}
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	public boolean insertar(DescuentoPelicula d) {
+	
+		if(existePorId(d.getId())) {
+			return false;
+		}
+		
+		String sql = "INSERT INTO descuento (id, codigo, porcentaje) VALUES (?, ?, ?)";
+		try (Connection conn= DBConnection.getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql)){
+			ps.setString(1, d.getId());
+			ps.setString(2, d.getCodigo());
+			ps.setDouble(3, d.getPorcentaje());
+			
+			ps.executeUpdate();
+			return true;
+						
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	
+	
+}
+
+}
