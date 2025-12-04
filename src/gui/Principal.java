d8e183353920bd475c1f5cbdc26251080106ff48
Iker Villarroya <i.villarroya@opendeusto.es>
2025-12-04 16:28:05 +0100

    compraEntradas(sin imagen)

--- src/gui/Principal.java
+++ src/gui/Principal.java
@@ -1,12 +1,17 @@
 package gui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import domain.Usuario;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.swing.JMenuBar;
+import javax.swing.JPanel;
 
 public class Principal extends JFrame{
 	
@@ -26,6 +31,22 @@ public class Principal extends JFrame{
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
+		
+		// Panel principal con los botones
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 40));
+		add(panel);
+
+		// Boton para ir a la pantalla de compra de entradas
+		JButton btnComprarEntradas = new JButton("Comprar entradas");
+		btnComprarEntradas.setPreferredSize(new Dimension(200, 40));
+		panel.add(btnComprarEntradas);
+
+		// Abre la ventana de compra de entradas
+		btnComprarEntradas.addActionListener(e -> {
+		    CompraEntradas ce = new CompraEntradas();
+		    ce.setVisible(true);
+		});
 		
 	}
 	
